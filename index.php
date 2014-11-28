c0b95638a9f6d008ca48431bb749947c9366b6f0
Bryan Leonard <b.leonard@zoho.com>
2014-11-27 22:45:19 -0500

    Initial commit of site redesign
    Just syncing up the goodies.

---
+++ index.php
@@ -0,0 +1,404 @@
+<!DOCTYPE html>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js"> <!--<![endif]-->
+	<head>
+		<!-- Meta Tags -->        
+		<meta charset="utf-8">
+		<!--[if IE]><meta http-equiv='X-UA-Compatible' content='IE=edge,chrome=1'><![endif]-->
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<title>Bryan Leonard - bryanleonard.us</title>
+		<meta name="description" content="The personal website of Bryan Leonard">
+		<meta name="keywords" content="bryan, leonard, bryan leonard, web, web design, web development, akron, cleveland, HTML5, Bootstrap 3, CSS3, javascript, jquery, responsive, mobile">
+		<meta name="author" content="Bryan Leonard">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=yes">
+		<!-- <link rel="shortcut icon" href="assets/img/favicon.ico" type="image/x-icon">  -->
+     
+		<link href='http://fonts.googleapis.com/css?family=Raleway:300,700' rel='stylesheet' type='text/css'>
+		<!-- <link href='http://fonts.googleapis.com/css?family=Raleway:400,100,300,500,700' rel='stylesheet' type='text/css'> -->
+		<link rel='stylesheet' href='http://fonts.googleapis.com/css?family=Open+Sans:400'>
+		<link rel='stylesheet' href='http://fonts.googleapis.com/css?family=Bree+Serif'>
+		<link rel="stylesheet" href="assets/plugins/bootstrap/css/bootstrap.min.css"> 
+
+		<link rel="stylesheet" href="assets/plugins/font-awesome/css/font-awesome.min.css">                          
+		<!-- <link rel="stylesheet" type="text/css" href="assets/plugins/revolution/css/style.css" media="screen"> -->
+		<!-- <link rel="stylesheet" type="text/css" href="assets/plugins/revolution/css/extralayers.css" media="screen">  -->
+		<!-- <link rel="stylesheet" type="text/css" href="assets/plugins/revolution/rs-plugin/css/settings.css" media="screen"> -->
+		<!-- <link rel="stylesheet" href="assets/plugins/owl/owl-carousel/owl.carousel.css"> -->
+		<!-- <link rel="stylesheet" href="assets/plugins/owl/owl-carousel/owl.theme.css"> -->
+		<!-- <link rel="stylesheet" href="assets/plugins/cube/cubeportfolio.min.css"> -->
+		<!-- <link rel="stylesheet" href="assets/plugins/jquery.mmenu.css"> -->
+         
+		<link rel="stylesheet" href="assets/css/style.css">
+	</head>
+	<body id="home" data-spy="scroll" data-target=".navbar">
+
+<!--Start Preloader -->
+<div id="preloader">
+	<div class="preloader-container">
+		<div class="spinner">
+			<div class="bounce1"></div>
+			<div class="bounce2"></div>
+			<div class="bounce3"></div>
+		</div>
+	</div>
+</div>
+<!-- End Preloader-->
+
+
+<!-- Big ole' hero image -->
+<section class="jumbo">
+	<div class="jumbo-content">
+		<h3 class="jumbo-text">
+			Hi, my name is Bryan. <span>I make <a href="#portfolio" class="jumbo-link js-scroll">websites</a>.</span>
+		</h3>
+		<a href="#about" class="btn black-btn js-scroll">Learn more about me</a>
+	</div>
+</section>
+<!-- /jumbo -->
+
+
+<!-- Navbar -->   
+<div id="header" class="header">            
+	<nav class="nav navbar navbar-default navbar-static header-nav header-nav-black" role="navigation">
+		<div class="container">
+			<!-- Brand and toggle get grouped for better mobile display -->
+			<div class="navbar-header">
+				<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#nav-menu">
+					<span class="sr-only">Toggle navigation</span>
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+				</button>
+				<!-- Logo -->        
+				<a class="navbar-brand" href="#home">Bryan Leonard</a>        
+			</div>
+			<!-- Collect the nav links, forms, and other content for toggling -->
+			<div class="collapse navbar-collapse" id="nav-menu">
+				<ul class="nav navbar-nav navbar-right">
+					<li class="active"><a href="#home" class="js-scroll">Home</a></li>
+					<li><a href="#about" class="js-scroll">About</a></li>                     
+					<li><a href="#portfolio" class="js-scroll">Portfolio</a></li>                    
+					<li><a href="#resume" class="js-scroll">Resume</a></li>
+					<li><a href="#contact" class="js-scroll">Contact</a></li>  
+				</ul>
+			</div><!-- /.navbar-collapse -->
+		</div><!-- /.container-fluid -->
+	</nav>
+</div>         
+<!-- Navbar -->
+
+
+<!-- Site Wrapper -->
+<div class="site-wrapper">
+
+	<!-- About -->            
+	<section class="padding-bottom padding-top-x2" id="about">
+		<div class="container">
+
+			<div class="general-title general-title-black text-center">
+				<h2>About Me</h2>
+				<div class="space-bottom-2x"></div>
+			</div>
+
+			<div class="row">                                 
+
+				<div class="col-sm-12 col-md-6 text-center">
+					<div class="team-inner">
+						<a href="#">  
+							<!-- Image -->                  
+							<img src="assets/img/ghost.gif" class="img-circle" alt="Bryan">                    
+							<div class="team-caption">
+								<!-- Social Icons -->
+								<div class="t-social-holder">
+									<div class="team-social team-social-black">                                           
+										<a href="https://github.com/bleonard1"><span class="fa-stack fa-lg"><i class="fa fa-circle fa-stack-2x"></i><i class="fa fa-github fa-stack-1x fa-inverse"></i></span></a>
+										<a href="https://twitter.com/shatteredrib"><span class="fa-stack fa-lg"><i class="fa fa-circle fa-stack-2x"></i><i class="fa my-fa-twtr fa-stack-1x fa-inverse"></i></span></a>
+										<a href="http://www.linkedin.com/in/bryantleonard/"><span class="fa-stack fa-lg"><i class="fa fa-circle fa-stack-2x"></i><i class="fa my-fa-lnkd fa-stack-1x fa-inverse"></i></span></a>
+										<a href="http://http://instagram.com/shatteredrib"><span class="fa-stack fa-lg"><i class="fa fa-circle fa-stack-2x"></i><i class="fa my-fa-insgm fa-stack-1x fa-inverse"></i></span></a>
+									</div>
+								</div>
+							</div>                                            
+						</a>
+					</div>                                    
+					
+				</div>
+
+				<div class="col-sm-12 col-md-6 padding-top">
+					<div class="team-details team-details-black">
+						<!-- <h5>The guy<h5> -->
+						<h3>Bryan Leonard</h3>   
+					</div>
+					<p>I'm a web designer and front-end developer out of Akron, Ohio.</p>
+
+					<p>After working at a Fortune 100 company for more than 12 years, I decided it was time for a change. I now work for <a href="http://www.dealertire.com">Dealer Tire, LLC</a> in Cleveland as a software developer and design lead within the team.</p>
+
+					<p>I am an experienced designer who specializes in clean, user-friendly and standards-compliant websites. With an eye for detail and a relentless pursuit for identifying and implementing process improvements, my work is widely respected amongst colleagues and clients. I was the lead web designer on a campaign that received recognition form the International Association of Business Communicators (IABC) for our work on growing awareness around laptop security in the workplace.</p>
+
+					<p>A long-time user of Photoshop, I can recall when the program only had a single undo option. I have spent countless hours writing HTML and CSS and have developed various applications with classic ASP and JavaScript. My current interests include creating cross-browser and smartphone friendly websites, growing the use of social media and I am currently trying my hand at PHP, JQuery and MySQL. Additionally, I am familiar with applications such as Dreamweaver, the AWeber and Constant Contact communication platforms, Interwoven TeamSite and the social media application IBM Connections.</p>
+
+					<p>In my spare time, I volunteer at a local animal shelter, regularly donate blood and am currently in the second tier discovery phase as a possible match for a patient in need of a bone marrow donation. I also enjoy reading classic fiction and play several instruments including piano, guitar and drums.</p>
+
+					<p>After starting my schooling at Baldwin-Wallace College, I obtained a BA in Business Management from Malone University and was a Dean’s List student graduating with a 3.92 GPA.</p>
+				</div>
+
+			</div><!-- /row -->
+
+		</div><!-- /container -->
+	</section>
+
+
+	<!-- Portfolio -->
+	<section class="bg-color padding-bottom padding-top-x2" id="portfolio">
+		<div class="container">
+
+		<div class="general-title general-title-black text-center">
+			<h2>Portfolio</h2>
+			<div class="space-bottom-2x"></div>
+		</div>
+
+
+Portfolio
+About<br>
+	About<br>
+	About<br>
+	About<br>
+	About<br>
+	About<br>
+	About<br>
+	About<br>
+	About<br>
+	About<br>
+	About<br>
+	About<br>
+	About<br>
+	About<br>
+	About<br>
+	About<br>
+	About<br>
+	About<br>
+	About<br>
+	About<br>
+	About<br>
+	About<br>
+	About<br>
+	About<br>
+	About<br>
+	About<br>
+	About<br>
+	About<br>
+	About<br>
+	About<br>
+
+
+		</div><!-- /container -->
+	</section>
+
+
+
+	<!-- Resume -->            
+	<section class="padding-bottom padding-top-x2" id="resume">
+		<div class="container">
+
+		<div class="general-title general-title-black text-center">
+			<h2>Resume</h2>
+			<div class="space-bottom-2x"></div>
+		</div>
+
+Resume
+About<br>
+	About<br>
+	About<br>
+	About<br>
+	About<br>
+	About<br>
+	About<br>
+	About<br>
+	About<br>
+	About<br>
+	About<br>
+	About<br>
+	About<br>
+	About<br>
+	About<br>
+	About<br>
+	About<br>
+	About<br>
+	About<br>
+	About<br>
+	About<br>
+	About<br>
+	About<br>
+	About<br>
+	About<br>
+	About<br>
+	About<br>
+	About<br>
+	About<br>
+	About<br>
+	About<br>
+	About<br>
+	About<br>
+	About<br>
+	About<br>
+	About<br>
+	About<br>
+	About<br>
+	About<br>
+	About<br>
+	About<br>
+	About<br>
+	About<br>
+	About<br>
+	About<br>
+
+		</div><!-- /container -->
+	</section>
+
+
+
+	<!-- Resume -->            
+	<section class="padding-bottom padding-top-x2" id="contact">
+		<div class="container">
+
+		<div class="general-title general-title-black text-center">
+			<h2>Contact</h2>
+			<div class="space-bottom-2x"></div>
+		</div>
+
+Contact
+About<br>
+	About<br>
+	About<br>
+	About<br>
+	About<br>
+	About<br>
+	About<br>
+	About<br>
+	About<br>
+	About<br>
+	About<br>
+	About<br>
+	About<br>
+	About<br>
+	About<br>
+	About<br>
+	About<br>
+	About<br>
+	About<br>
+	About<br>
+	About<br>
+	About<br>
+	About<br>
+	About<br>
+	About<br>
+	About<br>
+	About<br>
+	About<br>
+	About<br>
+	About<br>
+	About<br>
+	About<br>
+	About<br>
+	About<br>
+	About<br>
+	About<br>
+	About<br>
+	About<br>
+	About<br>
+	About<br>
+	About<br>
+	About<br>
+	About<br>
+	About<br>
+	About<br>
+
+			</div><!-- /container -->
+		</section>
+
+
+
+
+
+	<!-- Google Map (adress on map can be changed in app.js file) -->
+	<div id="map-canvas"></div>
+	<!-- End Google Map -->
+	<!-- Footer -->  
+	<div class="footer-big">
+		<div class="container">
+			<div class="row">
+
+				<!-- About Us Information -->
+				<div class="col-sm-6">                           
+					<div class="f-logo">
+						<h2>Location, location, location</h2>                                                                    
+					</div>
+				</div>
+				
+				<!-- Recent Posts -->
+				<div class="col-sm-6 f-inner f-inner-black">                        
+					<div class="col-sm-2 text-center">
+						<i class="fa fa-map-marker fa-lg"></i> 
+					</div>
+					<div class="col-sm-10">
+						<strong>Akron / Cleveland</strong>
+						<div class="f-space"></div>
+						<address>                                
+							<abbr title="Phone">P:</abbr> (330) 819-7515<br>
+							<abbr title="Email">E:</abbr> <a href='&#109;&#97;&#105;&#108;&#116;&#111;&#58;&#98;&#114;&#121;&#97;&#110;&#46;&#108;&#101;&#111;&#110;&#97;&#114;&#100;&#64;&#121;&#97;&#104;&#111;&#111;&#46;&#99;&#111;&#109;'>&#98;&#114;&#121;&#97;&#110;&#46;&#108;&#101;&#111;&#110;&#97;&#114;&#100;&#64;&#121;&#97;&#104;&#111;&#111;&#46;&#99;&#111;&#109;</a>
+						</address>                           
+					</div>
+				</div>
+
+			</div>
+		</div>
+	</div>   
+
+	<div id="footer">
+		<div class="container">   
+			<div class="row">                                                         
+				<!-- Copyright -->                                                       
+				<div class="col-sm-12 f-copyright">
+					<?php
+						date_default_timezone_set('America/New_York');
+						$today = date("Y"); 
+						echo "<span>All content &copy; " . $today . " Bryan Leonard unless <a href='#footer' class='credit-link js-credit'>otherwise noted</a>.</span>";
+					?>
+				</div>
+			</div><!-- /row -->
+		</div>
+	</div>
+	<!-- End Footer -->
+
+<div class="footer-big credits-row util-hidden">
+	<div class="container">
+		<div class="row padding-bottom padding-top">                                                         
+			<!-- Copyright -->                                                       
+			<div class="col-sm-12 f-copyright">
+				<p>Hero image courtesy of <a href="https://unsplash.com/ugmonk" class='credit-link'>Jeff Sheldon</a>, <a href="https://unsplash.com" class='credit-link'>Unsplash.com</a>.</p>
+			</div>
+		</div><!-- /row -->
+	</div><!-- /container -->
+</div>
+
+</div>
+<!-- /site-wrapper -->
+
+
+
+
+		<!-- Scripts and Plugins -->                                 
+		<script src="assets/plugins/jquery-1.11.1.min.js"></script>                    
+		<script src="assets/plugins/bootstrap/js/bootstrap.min.js"></script>
+		<!-- <script src="assets/plugins/revolution/rs-plugin/js/jquery.themepunch.tools.min.js"></script> -->
+		<!-- <script src="assets/plugins/revolution/rs-plugin/js/jquery.themepunch.revolution.min.js"></script> -->
+		<!-- <script src="assets/plugins/cube/jquery.cubeportfolio.min.js"></script> -->
+		<!-- <script src="assets/plugins/owl/owl-carousel/owl.carousel.js"></script> -->
+		<script src="assets/plugins/moderniz.js"></script>    
+		<script src="assets/plugins/jquery.sticky.js"></script>   
+		<!-- <script src="assets/plugins/jquery.mmenu.min.js"></script> -->
+		<script src="https://maps.googleapis.com/maps/api/js?v=3.exp"></script>     
+		<script src="assets/js/app.js"></script>                                
+	</body>
+</html>
